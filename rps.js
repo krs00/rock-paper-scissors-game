21350a6485f6ff1af66c430fffa34ad4652044c0
Kymani <kymanist00@gmail.com>
2022-10-22 00:52:16 -0700

    add func to play single round

--- rps.js
+++ rps.js
@@ -3,49 +3,85 @@
 // genrate random number 1-3 and assign each number to a string value
 
 function computerInput() {
-    let number = Math.floor(Math.random() * 3)+1;
-    if (number === 1) {
+    let random = Math.floor(Math.random() * 3)+1;
+    if (random === 1) {
         return "rock"; 
     }
-    else if (number === 2) {
+    else if (random === 2) {
         return "paper";
     }
-    else if (number === 3) {
+    else if (random === 3) {
         return "scissors"
-    }
-
+    }  
 } 
 // Get userChoice
 // User choice must be converted to lower case to neutralize string input
 // User input must be validated as a legal option to return choice (through if else)
 
-function playerInput() { 
+function playerInput() {  
     let playerInput = prompt("Choose rock, paper or scissors!").toLowerCase();
     if (playerInput === "rock" || playerInput === "paper" || playerInput === "scissors") 
-    return playerInput
+        return playerInput;
     else
-    return "That is not a valid input!"
+        console.log("Please choose rock, paper or scissors!")
 }
     
 // Store userInput and computerInput inside variables
-const computerSelection = computerInput();
-const playerSelection = playerInput(); 
+
+const computerChoice = computerInput();
+const playerChoice = playerInput();
+
+
 
 // Write function that plays a single round of rock paper scissors
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return `It's a tie! you both chose ${playerSelection}!`; 
+// IF THE PLAYER WINS 
+function playRound() {
+    if (playerChoice === computerChoice) {
+        console.log(`The computer chose ${computerChoice}`);
+        console.log(`You chose ${playerChoice}`);  
+        console.log("It's a tie!"); 
     }
-    else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win! rock beats scissors!";
+    else if (playerChoice == 'rock' && computerChoice == 'scissors') {
+        console.log(`The computer chose ${computerChoice}`);
+        console.log(`You chose ${playerChoice}`);  
+        console.log("You win! Rock beats scissors!"); 
     }
-    else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win! paper beats rock!";
+    else if (playerChoice == 'paper' && computerChoice == 'rock') {
+        console.log(`The computer chose ${computerChoice}`);
+        console.log(`You chose ${playerChoice}`);  
+        console.log("You win! Paper beats rock!"); 
     }
-    else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win! scissors beats paper!";
+    else if (playerChoice == 'scissors' && computerChoice == 'paper') {
+        console.log(`The computer chose ${computerChoice}`);
+        console.log(`You chose ${playerChoice}`);  
+        console.log("You win! Scissors beats paper!");  
     }
-    else
-        return `You lose! ${computerSelection} beats ${playerSelection}!`
+    // IF THE PLAYER ENTERS UNDEFINED VALUE 
+    else if (playerChoice === undefined) { 
+        return
+    }
+    // IF THE PLAYER LOOSES
+    else if (playerChoice == 'rock' && computerChoice == 'paper') {
+        console.log(`The computer chose ${computerChoice}`);
+        console.log(`You chose ${playerChoice}`);  
+        console.log("You loose! Paper beats rock!");
 }
+    else if (playerChoice == 'paper' && computerChoice == 'scissors') {
+        console.log(`The computer chose ${computerChoice}`);
+        console.log(`You chose ${playerChoice}`);  
+        console.log("You loose! Scissors beats paper!");
+}
+    else if (playerChoice == 'scissors' && computerChoice == 'rock') {
+        console.log(`The computer chose ${computerChoice}`);
+        console.log(`You chose ${playerChoice}`);  
+        console.log("You loose! Rock beats scissors!"); 
+}
+
+}
+
+playRound();
+
+
+
+ 
