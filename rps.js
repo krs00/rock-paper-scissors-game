763cbaf5242abf9ddeb50c06a4183ea948ea16f7
Kymani <kymanist00@gmail.com>
2022-10-21 22:06:08 -0700

    add function to play single round

--- rps.js
+++ rps.js
@@ -14,19 +14,38 @@ function computerInput() {
         return "scissors"
     }
 
-}
-
+} 
 // Get userChoice
 // User choice must be converted to lower case to neutralize string input
 // User input must be validated as a legal option to return choice (through if else)
 
-function playerInput() {
+function playerInput() { 
     let playerInput = prompt("Choose rock, paper or scissors!").toLowerCase();
     if (playerInput === "rock" || playerInput === "paper" || playerInput === "scissors") 
     return playerInput
     else
-    console.log("That is not a valid input!")
-    return ""
+    return "That is not a valid input!"
 }
     
-console.log(playerInput())
+// Store userInput and computerInput inside variables
+const computerSelection = computerInput();
+const playerSelection = playerInput(); 
+
+// Write function that plays a single round of rock paper scissors
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a tie! you both chose ${playerSelection}!`; 
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! rock beats scissors!";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! paper beats rock!";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! scissors beats paper!";
+    }
+    else
+        return `You lose! ${computerSelection} beats ${playerSelection}!`
+}
