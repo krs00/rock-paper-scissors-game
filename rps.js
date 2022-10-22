8881c5cdcbc5e18ef3fd09b34858ab65df5ccbb0
Kymani <kymanist00@gmail.com>
2022-10-21 21:51:05 -0700

    add user choice function

--- rps.js
+++ rps.js
@@ -1,17 +1,17 @@
 // Get computerChoice
-// Write a funciton or variable that changes between rock, paper or scissors for computerChoice
+// Write a funciton that changes between rock, paper or scissors for computerChoice
 // genrate random number 1-3 and assign each number to a string value
 
-function computerChoice() {
+function computerInput() {
     let number = Math.floor(Math.random() * 3)+1;
     if (number === 1) {
-        return "rock";
+        return "rock"; 
     }
     else if (number === 2) {
         return "paper";
     }
     else if (number === 3) {
-        return "scissors" 
+        return "scissors"
     }
 
 }
@@ -20,15 +20,13 @@ function computerChoice() {
 // User choice must be converted to lower case to neutralize string input
 // User input must be validated as a legal option to return choice (through if else)
 
-function playerChoice() {
-    let playerChoice = prompt("Choose rock paper, or scissors!").toLowerCase();
-    if (playerChoice === "rock" ||  playerChoice === "paper" || playerChoice === "scissors" )
-    return;
+function playerInput() {
+    let playerInput = prompt("Choose rock, paper or scissors!").toLowerCase();
+    if (playerInput === "rock" || playerInput === "paper" || playerInput === "scissors") 
+    return playerInput
     else
-    console.log("Please type in rock, paper or scissors!")
+    console.log("That is not a valid input!")
+    return ""
 }
-
-// Initialize score variables for player and computer
-
-let playerScore = 0;
-let computerScore = 0;
+    
+console.log(playerInput())
